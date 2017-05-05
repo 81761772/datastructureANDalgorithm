2a178caf5599f2b0a410600845fa3f7cb2aa92aa
Yao Chao <81761772@qq.com>
2017-05-05 19:12:53 +0800

    Add files via upload

---
+++ SeqSearch.java
@@ -0,0 +1,18 @@
+package study;
+
+public class SeqSearch {
+	
+	public int SeqSearch(int first,int last,int target,int[] arr){
+		for(int i = first;i<last;i++){
+			if(arr[i]==target){
+				return i;
+			}
+		}
+		return -1;//注意此处用法
+	}
+	public static void main(String[] args){
+		int[] arr = {25,36,36,47,15};
+		SeqSearch ss = new SeqSearch();
+		System.out.println(ss.SeqSearch(0,arr.length,47,arr));
+	}
+}
