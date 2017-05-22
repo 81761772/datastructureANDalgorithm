a80eccd639313fb4d69863464f45492c11c30aa4
Yao Chao <81761772@qq.com>
2017-05-22 20:47:50 +0800

    Add files via upload

---
+++ Recursion_Factorial.java
@@ -0,0 +1,16 @@
+package study;
+
+public class Recursion_Factorial {
+	
+	public static int factorial(int n){
+		if(n==0){
+			return 1;
+		}else{
+			return n*factorial(n-1);
+		}
+	}
+	
+	public static void main(String[] args){
+		System.out.println(factorial(3));
+	}
+}
