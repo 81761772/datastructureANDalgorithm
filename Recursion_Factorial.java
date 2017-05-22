d8499b37a7929e765c471fdf9a90665109fe6afa
Yao Chao <81761772@qq.com>
2017-05-22 21:08:40 +0800

    Update Recursion_Factorial.java

--- Recursion_Factorial.java
+++ Recursion_Factorial.java
@@ -3,7 +3,7 @@ package study;
 public class Recursion_Factorial {
 	
 	public static int factorial(int n){
-		if(n==0){
+		if(n==0){//数学中规定0的阶乘为1
 			return 1;
 		}else{
 			return n*factorial(n-1);
